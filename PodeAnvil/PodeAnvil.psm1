b8c58bd2ad09c6d284465bd4887792481bab6a3e
Avri Chen-Roth <avri210984@gmail.com>
2021-09-07 22:44:43 +0300

    Create PodeAnvil.psm1 and PodeAnvil.psd1

---
+++ PodeAnvil/PodeAnvil.psm1
@@ -0,0 +1,13 @@
+# Dot source public/private functions
+$public = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Public/*.ps1') -Recurse -ErrorAction Stop)
+#$private = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Private/*.ps1') -Recurse -ErrorAction Stop)
+$private = @()
+
+foreach ($import in @($public + $private)) {
+    try {
+        . $import.FullName
+    }
+    catch {
+        throw "Unable to dot source [$($import.FullName)]"
+    }
+}
