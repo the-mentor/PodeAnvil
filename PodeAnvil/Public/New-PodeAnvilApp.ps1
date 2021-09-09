5a4d0d1ec3015bca6d53b08e275237796c76b68f
Avri Chen-Roth <avri210984@gmail.com>
2021-09-09 21:46:33 +0300

    remove begin/process/end

--- PodeAnvil/Public/New-PodeAnvilApp.ps1
+++ PodeAnvil/Public/New-PodeAnvilApp.ps1
@@ -29,109 +29,103 @@ function New-PodeAnvilApp {
         [Switch]$Force
     )
 
-    BEGIN {
-        $ModulePath = (Get-Item $PSScriptRoot).parent
+    $ModulePath = (Get-Item $PSScriptRoot).parent
 
-        if ($IsLinux -or $IsMacOS) {
-            $PowerShellExecutable = 'pwsh'
-        }
-        else {
-            if ($null -eq $PowerShellExecutable) {
-                $PowerShellExecutable = 'powershell'
-            }
-        }
-
-        #check if index.js exists
-        $IndexJsPath = (Join-Path -Path $ModulePath.FullName -ChildPath "index.js")
-        if (Test-Path -Path $IndexJsPath -ErrorAction Stop) {
-
-        }
-
-        #check if webserver.json exists and read its content
-        $WebServerConfigPath = (Join-Path -Path $ModulePath.FullName -ChildPath "webserver.json")
-        if (Test-Path -Path $WebServerConfigPath -ErrorAction Stop) {
-            Write-Verbose "Reading WebServerConfig from $WebServerConfigPath"
-            $WebServerConfig = Get-Content -Path $WebServerConfigPath -ErrorAction Stop | ConvertFrom-Json -ErrorAction Stop
-        }
-
-        if (Test-Path -Path $Path) {
-            $ps1file = Get-Item -Path $Path
-            if ($ps1file.Name -notlike '*.ps1') {
-                throw "Please specify a ps1 file in `$Path ($Path)"
-            }
-        }
-        else {
-            throw
-        }
-
-        $Npx = Get-Command npx
-        if ($null -eq $Npx) {
-            throw "NodeJS is required to run New-PodeAnvilApp you can download it here: https://nodejs.org"
-        }
-
-        $Git = Get-Command git
-        if ($null -eq $Git) {
-            throw "Git is required to run New-PodeAnvilApp."
-        }
-
-        if ($null -eq $OutputPath) {
-            $OutputPath = $PWD
-            Write-Verbose "No output path specified. Using: $OutputPath"
-        }
-        else {
-            $OutputPath = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($OutputPath)
-            Write-Verbose "Output path resolved to: $OutputPath"
-        }
-
-        if (Test-Path (Join-Path $OutputPath $Name)) {
-            Write-Verbose "Output path exists. Removing it."
-            Remove-Item (Join-Path $OutputPath $Name) -Force -Recurse
-        }
-
-        if (-not (Test-Path $OutputPath)) {
-            Write-Verbose "Output path does not exist. Creating new output path."
-            New-Item -Path $OutputPath -ItemType Directory | Out-Null
+    if ($IsLinux -or $IsMacOS) {
+        $PowerShellExecutable = 'pwsh'
+    }
+    else {
+        if ($null -eq $PowerShellExecutable) {
+            $PowerShellExecutable = 'powershell'
         }
     }
 
-    PROCESS {
-        Push-Location $OutputPath
-
-        Write-Verbose "Installing: create-electron-app@latest"
-        npm install create-electron-app #--global
-
-        Write-Verbose "Creating electron app: $Name"
-        npx create-electron-app $Name
-
-        Pop-Location
-
-        $ElectronSourceDir = [IO.Path]::Combine($OutputPath, $Name, 'src')
-        Write-Verbose "ElectronSourceDir: $ElectronSourceDir"
-
-        Write-Verbose "Copy Pode PS1 File $ps1file"
-        Copy-Item -Path $ps1file -Destination $((Join-Path -Path $ElectronSourceDir -ChildPath $ps1file.Name))
-
-        #copy Index.js file to the Electron Source Directory
-        Copy-Item -Path $IndexJsPath -Destination $((Join-Path -Path $ElectronSourceDir -ChildPath "index.js")) -Force
-
-        Copy-PodeModules -Destination $ElectronSourceDir
-
-        #Modify Web Server Config
-        $WebServerConfig.powershell_exec = $PowerShellExecutable
-        $WebServerConfig.port = $Port
-        $WebServerConfig.pode_script_filename = $ps1file.Name
-
-        #Create Web Server Config JSON file
-        $WebServerConfig | ConvertTo-Json | Set-Content -Path "$ElectronSourceDir\webserver.json" -Force
-
-        Write-Verbose "Building electron app with forge"
-        npm install @electron-forge/cli
-
-        Set-Location (Join-Path -Path $OutputPath -ChildPath $Name)
-        electron-forge make
-    }
-
-    END {
+    #check if index.js exists
+    $IndexJsPath = (Join-Path -Path $ModulePath.FullName -ChildPath "index.js")
+    if (Test-Path -Path $IndexJsPath -ErrorAction Stop) {
 
     }
+
+    #check if webserver.json exists and read its content
+    $WebServerConfigPath = (Join-Path -Path $ModulePath.FullName -ChildPath "webserver.json")
+    if (Test-Path -Path $WebServerConfigPath -ErrorAction Stop) {
+        Write-Verbose "Reading WebServerConfig from $WebServerConfigPath"
+        $WebServerConfig = Get-Content -Path $WebServerConfigPath -ErrorAction Stop | ConvertFrom-Json -ErrorAction Stop
+    }
+
+    if (Test-Path -Path $Path) {
+        $ps1file = Get-Item -Path $Path
+        if ($ps1file.Name -notlike '*.ps1') {
+            throw "Please specify a ps1 file in `$Path ($Path)"
+        }
+    }
+    else {
+        throw
+    }
+
+    $Npx = Get-Command npx
+    if ($null -eq $Npx) {
+        throw "NodeJS is required to run New-PodeAnvilApp you can download it here: https://nodejs.org"
+    }
+
+    $Git = Get-Command git
+    if ($null -eq $Git) {
+        throw "Git is required to run New-PodeAnvilApp."
+    }
+
+    if ($null -eq $OutputPath) {
+        $OutputPath = $PWD
+        Write-Verbose "No output path specified. Using: $OutputPath"
+    }
+    else {
+        $OutputPath = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($OutputPath)
+        Write-Verbose "Output path resolved to: $OutputPath"
+    }
+
+    if (Test-Path (Join-Path $OutputPath $Name)) {
+        Write-Verbose "Output path exists. Removing it."
+        Remove-Item (Join-Path $OutputPath $Name) -Force -Recurse
+    }
+
+    if (-not (Test-Path $OutputPath)) {
+        Write-Verbose "Output path does not exist. Creating new output path."
+        New-Item -Path $OutputPath -ItemType Directory | Out-Null
+    }
+
+
+
+    Push-Location $OutputPath
+
+    Write-Verbose "Installing: create-electron-app@latest"
+    npm install create-electron-app #--global
+
+    Write-Verbose "Creating electron app: $Name"
+    npx create-electron-app $Name
+
+    Pop-Location
+
+    $ElectronSourceDir = [IO.Path]::Combine($OutputPath, $Name, 'src')
+    Write-Verbose "ElectronSourceDir: $ElectronSourceDir"
+
+    Write-Verbose "Copy Pode PS1 File $ps1file"
+    Copy-Item -Path $ps1file -Destination $((Join-Path -Path $ElectronSourceDir -ChildPath $ps1file.Name))
+
+    #copy Index.js file to the Electron Source Directory
+    Copy-Item -Path $IndexJsPath -Destination $((Join-Path -Path $ElectronSourceDir -ChildPath "index.js")) -Force
+
+    Copy-PodeModules -Destination $ElectronSourceDir
+
+    #Modify Web Server Config
+    $WebServerConfig.powershell_exec = $PowerShellExecutable
+    $WebServerConfig.port = $Port
+    $WebServerConfig.pode_script_filename = $ps1file.Name
+
+    #Create Web Server Config JSON file
+    $WebServerConfig | ConvertTo-Json | Set-Content -Path "$ElectronSourceDir\webserver.json" -Force
+
+    Write-Verbose "Building electron app with forge"
+    npm install @electron-forge/cli
+
+    Set-Location (Join-Path -Path $OutputPath -ChildPath $Name)
+    electron-forge make
 }
